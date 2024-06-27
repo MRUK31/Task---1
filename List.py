741c6f37cc6769a147e2f69d8a03008f4344554e
MRUK31 <mrunalipradeepkale@gmail.com>
2024-06-27 18:54:37 +0530

    Initial commit

---
+++ List.py
@@ -0,0 +1,17 @@
+#Create a list
+MK_list = [1, 2, 3, 4]
+
+#To add an element in the list
+MK_list.append(5)
+
+#To remove an element from the list
+MK_list.remove(2)
+
+#Modifying the element
+MK_list[0] = 10
+
+#inserting an element (changing the element value to a new value)
+MK_list.insert(2, "TWO")
+
+#To print the list
+print("Updated list:", MK_list)
